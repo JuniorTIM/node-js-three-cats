012bfdbdc01e82b3043f35d281863cad051e6d29
JuniorTIM <musa.aliev06@yandex.ru>
2022-05-23 14:59:42 +0300

    Три кота успешно печатаются

---
+++ index.js
@@ -0,0 +1,11 @@
+import catMe from 'cat-me'
+function threeCats() {
+    let three; 
+    for (let i = 0; i < 3; i++) {
+    three = catMe()
+    three = catMe()
+    three = catMe()
+    console.log(three)
+}
+}
+threeCats()
